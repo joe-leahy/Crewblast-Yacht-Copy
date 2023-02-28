bdc403e58b375a6e99c546795ef23827f9f3cf00
joe-leahy <josleahy@icloud.com>
2023-02-27 19:23:35 -0500

    email link

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,59 +4,61 @@ import { motion } from "framer-motion";
 
 const Header = () => {
   return (
-      <header className="sticky top-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
-        <motion.div
-          initial={{
-            x: -500,
-            opacity: 0,
-            scale: 0.5,
-          }}
-          animate={{
-            x: 0,
-            opacity: 1,
-            scale: 1,
-          }}
-          transition={{
-            duration: 1,
-          }}
-          className="flex flex-row items-center"
-        >
-          <SocialIcon
-            url="https://www.joeleahycreative.com/"
-            fgColor="white"
-            bgColor="transparent"
-            network="squarespace"
-          />
-          <SocialIcon
-            url="https://www.linkedin.com/company/crewblast/"
-            fgColor="white"
-            bgColor="transparent"
-          />
-          <SocialIcon
-            url="https://www.instagram.com/crewblast/"
-            fgColor="white"
-            bgColor="transparent"
-          />
-        </motion.div>
+    <header className="sticky top-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
+      <motion.div
+        initial={{
+          x: -500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1,
+        }}
+        className="flex flex-row items-center"
+      >
+        <SocialIcon
+          url="https://www.crewblast.co/"
+          fgColor="white"
+          bgColor="transparent"
+          network="telegram"
+        />
+        <SocialIcon
+          url="https://www.linkedin.com/company/crewblast/"
+          fgColor="white"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://www.instagram.com/crewblast/"
+          fgColor="white"
+          bgColor="transparent"
+        />
+      </motion.div>
 
-        <motion.div
-          initial={{
-            x: 500,
-            opacity: 0,
-            scale: 0.5,
-          }}
-          animate={{
-            x: 0,
-            opacity: 1,
-            scale: 1,
-          }}
-          transition={{ duration: 1 }}
-          className="flex flex-row items-center text-gray-300 cursor-pointer"
-        >
+      <motion.div
+        initial={{
+          x: 500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{ duration: 1 }}
+        className="flex flex-row items-center text-gray-300 cursor-pointer"
+      >
+        <a href="mailto:jleahy@crewblastyacht.com">
           <SocialIcon network="email" fgColor="white" bgColor="transparent" />
           <p className="hidden md:inline-flex text-sm white">Contact Us</p>
-        </motion.div>
-      </header>
+        </a>
+      </motion.div>
+    </header>
   );
 };
 
