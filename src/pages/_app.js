637f8b628e478d88e19b94204611d0625645305a
joe-leahy <josleahy@icloud.com>
2023-03-07 12:35:28 -0500

    added google analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,34 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import Script from "next/script";
 
 const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
-}
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXX"
+      />
+      <Script
+      id='google-analytics'
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-TKTYJKD5MW', {
+            page_path: window.location.pathname,
+          });
+        `,
+        }}
+    />
+      <Component {...pageProps} />
+    </>
+  );
+};
 
 App.getInitialProps = () => {
   return {};
 };
 
-export default App
+export default App;
