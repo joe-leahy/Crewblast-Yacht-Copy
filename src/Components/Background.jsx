1f23fadd6b2c4d259d1b911de6323eaf29d70a54
joe-leahy <josleahy@icloud.com>
2023-03-07 20:01:59 -0500

    backgroundImages

--- src/Components/Background.jsx
+++ src/Components/Background.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const Background = () => {
   return (
     <div
-      className="absolute h-screen w-screen bg-center bg-contain bg-[url('/MobileBack.jpg')] opacity-[.12] z-0 sm:bg-[url('/Background.jpg')] sm:bg-auto"
+      className="absolute h-screen w-screen bg-center bg-cover bg-contain bg-[url('/MobileBack.jpg')] opacity-[.12] z-0 sm:bg-[url('/Background.jpg')]"
     ></div>
   );
 };
